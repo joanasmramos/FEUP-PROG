72247d9ff3d1a552c021121307ea6d8e9e02df5b
Gonçalo Nuno Bernardo <goncalonunobernardo@gmail.com>
2017-05-15 12:14:08 +0100

    Reparos de erros da main

--- main Gonçalo.cpp
+++ main Gonçalo.cpp
@@ -70,7 +70,7 @@ void SaveFileDrivers() {
 
 	for (unsigned int i = 0; i < Drivers.size(); i++)
 	{
-		FicheiroCondutores << Drivers.at(i).getId << " ; " << Drivers.at(i).getName << " ; " << Drivers.at(i).getShiftMaxDuration << " ; " << Drivers.at(i).getMaxWeekWorkingTime << " ; " << Drivers.at(i).getMinRestTime << endl;
+		FicheiroCondutores << Drivers.at(i).getId() << " ; " << Drivers.at(i).getName() << " ; " << Drivers.at(i).getShiftMaxDuration() << " ; " << Drivers.at(i).getMaxWeekWorkingTime() << " ; " << Drivers.at(i).getMinRestTime() << endl;
 	}
 	FicheiroCondutores.close();
 	cout << "\n\nFicheiro gravado! Retornando ao Menu Principal...\n";
@@ -89,26 +89,26 @@ void SaveFileLines() {
 
 	for (unsigned int i = 0; i < Lines.size(); i++)
 	{
-		FicheiroLinhas << Lines.at(i).getId << " ; " << Lines.at(i).getFreq << " ; ";
-		for (unsigned int j = 0; j < Lines.at(i).getBusStops.size(); j++)
+		FicheiroLinhas << Lines.at(i).getId() << " ; " << Lines.at(i).getFreq() << " ; ";
+		for (unsigned int j = 0; j < Lines.at(i).getBusStops().size(); j++)
 		{
-			if (j == Lines.at(i).getBusStops.size() - 1)
+			if (j == Lines.at(i).getBusStops().size() - 1)
 			{
-				FicheiroLinhas << Lines.at(i).getBusStops.at(j) << "; ";
+				FicheiroLinhas << Lines.at(i).getBusStops().at(j) << "; ";
 			}
 			else {
-				FicheiroLinhas << Lines.at(i).getBusStops.at(j) << ", ";
+				FicheiroLinhas << Lines.at(i).getBusStops().at(j) << ", ";
 			}
 		}
-		for (unsigned int k = 0; k < Lines.at(i).getTimings.size(); k++)
+		for (unsigned int k = 0; k < Lines.at(i).getTimings().size(); k++)
 		{
-			if (k == Lines.at(i).getTimings.size() - 1)
+			if (k == Lines.at(i).getTimings().size() - 1)
 			{
-				FicheiroLinhas << Lines.at(i).getTimings.at(k) << "";
+				FicheiroLinhas << Lines.at(i).getTimings().at(k) << "";
 			}
 
 			else {
-				FicheiroLinhas << Lines.at(i).getTimings.at(k) << ", ";
+				FicheiroLinhas << Lines.at(i).getTimings().at(k) << ", ";
 			}
 
 		}
@@ -156,7 +156,7 @@ void ScheduleStops() {
 		}
 		else {
 			for (unsigned int i = 0; i < Lines.size(); i++) {
-				if (Lines.at(i).getId == id) {
+				if (Lines.at(i).getId() == id) {
 					flag = 1; // YAY!
 					indice = i;
 				}
@@ -173,14 +173,14 @@ void ScheduleStops() {
 	cout << "========================================================================================================\n";
 
 	unsigned int opcao;
-	for (unsigned int i = 0; i < Lines.at(indice).getBusStops.size(); i++) {
-		cout << i << " - " << Lines.at(indice).getBusStops.at(i) << endl;
+	for (unsigned int i = 0; i < Lines.at(indice).getBusStops().size(); i++) {
+		cout << i << " - " << Lines.at(indice).getBusStops().at(i) << endl;
 	}
 	//verifying if the input is valid
 	while (true) {
 		cout << "Introduza a paragem que deseja:\n";
 		cin >> opcao;
-		if (cin.fail() || (opcao >= Lines.at(indice).getBusStops.size()) || (opcao < 0)) {
+		if (cin.fail() || (opcao >= Lines.at(indice).getBusStops().size()) || (opcao < 0)) {
 			cout << endl << "Invalido!" << endl;
 			cin.clear();
 			cin.ignore(100000, '\n');
@@ -191,7 +191,7 @@ void ScheduleStops() {
 	}
 
 	for (unsigned int i = 0; i < opcao; i++) {
-		minutos = minutos + Lines.at(indice).getTimings.at(i);
+		minutos = minutos + Lines.at(indice).getTimings().at(i);
 		if (minutos >= 60) {
 			minutos = minutos - 60;
 			horas = horas + 1; //I mean...
@@ -200,16 +200,16 @@ void ScheduleStops() {
 	cout << "HORARIO DESEJADO: (HORAS || ID da LINHA)\n";
 	cout << "========================================================================================================\n";
 	cout << horas << ":" << setfill('0') << setw(2);
-	cout << minutos << "   " << Lines.at(indice).getId << endl;
+	cout << minutos << "   " << Lines.at(indice).getId() << endl;
 	cout << "========================================================================================================\n";
 	for (unsigned int i = 0; i < 10; i++) {
-		minutos = minutos + Lines.at(indice).getFreq;
+		minutos = minutos + Lines.at(indice).getFreq();
 		if (minutos >= 60) {
 			minutos = minutos - 60;
 			horas = horas + 1;
 		}
 		cout <<"========================================================================================================\n";
-		cout << horas << ":" << setfill('0') << setw(2) << minutos << "   " << Lines.at(indice).getId<< endl;
+		cout << horas << ":" << setfill('0') << setw(2) << minutos << "   " << Lines.at(indice).getId()<< endl;
 		cout <<"========================================================================================================\n";
 	}
 	cout << "Prima 0 para retornar ao Menu Principal...\n";
@@ -225,7 +225,7 @@ void ScheduleLines() {
 	int flag = 0;
 	int indice = 0;
 	int input = -1;
-	int horas = 13;
+	int horas = 13; //Begins at 1:00pm
 	int minutos = 0;
 	while (true) {
 		cout << "Introduza o ID da linha que deseja: ";
@@ -239,7 +239,7 @@ void ScheduleLines() {
 		else {
 			
 			for (unsigned int i = 0; i < Lines.size(); i++) {
-				if (Lines.at(i).getId == id) {
+				if (Lines.at(i).getId() == id) {
 					flag = 1;
 					indice = i;//YAYX2
 				}
@@ -254,17 +254,17 @@ void ScheduleLines() {
 	}
 	cout << "HORARIO DESEJADO: (HORAS || PARAGENS)\n";
 	cout << "========================================================================================================\n";
-	cout << horas << ":" << setfill('0') << setw(2) << minutos << "   " << Lines.at(indice).getBusStops.at(0) << endl;
+	cout << horas << ":" << setfill('0') << setw(2) << minutos << "   " << Lines.at(indice).getBusStops().at(0) << endl;
 	cout << "========================================================================================================\n";
 
-	for (int n = 1; n < Lines.at(indice).getBusStops.size(); n++) {
-		minutos = minutos + Lines.at(indice).getTimings.at(n - 1);
+	for (unsigned int n = 1; n < Lines.at(indice).getBusStops().size(); n++) {
+		minutos = minutos + Lines.at(indice).getTimings().at(n - 1);
 		if (minutos >= 60) {
 			minutos = minutos - 60;
 			horas = horas + 1;
 		}
 		cout << "========================================================================================================\n";
-		cout << horas << ":" << setfill('0') << setw(2) << minutos << "   " << Lines.at(indice).getBusStops.at(n) << endl;
+		cout << horas << ":" << setfill('0') << setw(2) << minutos << "   " << Lines.at(indice).getBusStops().at(n) << endl;
 		cout << "========================================================================================================\n";
 	}
 	cout << "Prima 0 para retornar ao Menu Principal...\n";
@@ -288,16 +288,13 @@ void AddDriver() {
 	cout << "========================================================================================================\n";
 	cout << "Indique o ID do motorista a adicionar: " << endl;
 	cin >> tempid;
-	//cin >> tempCondutor.setId;
-	//não percebi muito bem o argumento da ErrorErrorError... tempCondutor.setId retorna void sempre porque é um método set
-	//ErrorErrorError(tempCondutor.setId);
 	ErrorErrorError(tempid);
 	tempCondutor.setId(tempid);
 
 	cin.ignore(1000, '\n');
 	cout << "Indique o nome do motorista:  " << endl;
 	getline(cin, Nome);
-	Drivers.at(RetIDDrivers(tempCondutor.getId)).setName(Nome);
+	Drivers.at(RetIDDrivers(tempCondutor.getId())).setName(Nome);
 
 	cout << "Indique o numero máximo de horas do turno do motorista:\n";
 	cin >> tempmaxhours;
@@ -508,14 +505,14 @@ void CalcTimeStops() {
 	{
 		int par1 = 0;
 		int par2 = 0;
-		for (unsigned int j = 0; j < Lines.at(i).getBusStops.size(); j++)
+		for (unsigned int j = 0; j < Lines.at(i).getBusStops().size(); j++)
 		{
-			if (Lines.at(i).getBusStops.at(j) == parg1)
+			if (Lines.at(i).getBusStops().at(j) == parg1)
 			{
 				par1 = j;
 			}
 
-			if (Lines.at(i).getBusStops.at(j) == parg2)
+			if (Lines.at(i).getBusStops().at(j) == parg2)
 			{
 				par2 = j;
 			}
@@ -526,19 +523,19 @@ void CalcTimeStops() {
 			if (par1 > par2)
 			{
 				cout << "========================================================================================================\n";
-				cout << "SENTIDO: " << Lines.at(i).getBusStops.at(0); //Unidirectional
+				cout << "SENTIDO: " << Lines.at(i).getBusStops().at(0); //Unidirectional
 			}
 
 			else
 			{
 				cout << "========================================================================================================\n";
-				cout << "SENTIDO: " << Lines.at(i).getBusStops.at(Lines.at(i).getBusStops.size() - 1); //Coming back
+				cout << "SENTIDO: " << Lines.at(i).getBusStops().at(Lines.at(i).getBusStops().size() - 1); //Coming back
 
 			}
 			int tempo = 0;
-			for (int k = (par1 > par2 ? par2 : par1); k < (par1 < par2 ? par2 : par1); k++) // iterar do par mais pequeno ao maior, k sendo a posicao no vetor tempo
+			for (int k = (par1 > par2 ? par2 : par1); k < (par1 < par2 ? par2 : par1); k++) // Iterating of the smallest pair to the biggest, being k the position in the time vector
 			{
-				tempo += Lines.at(i).getTimings.at(k);
+				tempo += Lines.at(i).getTimings().at(k);
 			}
 
 			cout << endl;
@@ -564,11 +561,11 @@ void SeeExistingStops() {
 	cout << "\nPERCURSOS EXISTENTES: \n\n";
 	for (unsigned int i = 0; i < Lines.size(); i++) {
 
-		for (unsigned int f = 0; f < Lines.at(i).getBusStops.size(); f++) {
+		for (unsigned int f = 0; f < Lines.at(i).getBusStops().size(); f++) {
 
-			cout << "||ID - " << Lines.at(i).getId << "|| ";
+			cout << "||ID - " << Lines.at(i).getId() << "|| ";
 
-			cout << Lines.at(i).getBusStops.at(f);
+			cout << Lines.at(i).getBusStops().at(f);
 
 			cout << ";\n";
 		}
@@ -585,17 +582,17 @@ void SeeExistingLines() {
 
 	cout << "LINHAS EXISTENTES: \n\n";
 	for (unsigned int i = 0; i < Lines.size(); i++) {
-		cout << "||NR - " << (i + 1) << "|| " << Lines.at(i).getId << " ; " << Lines.at(i).getFreq << " ; ";
-		for (unsigned int j = 0; j < Lines.at(i).getBusStops.size(); j++) {
-			cout << Lines.at(i).getBusStops[j];
-			if ((j + 1) < Lines.at(i).getBusStops.size()) {
+		cout << "||NR - " << (i + 1) << "|| " << Lines.at(i).getId() << " ; " << Lines.at(i).getFreq() << " ; ";
+		for (unsigned int j = 0; j < Lines.at(i).getBusStops().size(); j++) {
+			cout << Lines.at(i).getBusStops().at(j);
+			if ((j + 1) < Lines.at(i).getBusStops().size()) {
 				cout << ", ";
 			}
 		}
 		cout << "; ";
-		for (unsigned int j = 0; j < Lines.at(i).getTimings.size(); j++) {
-			cout << Lines.at(i).getTimings[j];
-			if ((j + 1) < Lines.at(i).getTimings.size())
+		for (unsigned int j = 0; j < Lines.at(i).getTimings().size(); j++) {
+			cout << Lines.at(i).getTimings().at(j);
+			if ((j + 1) < Lines.at(i).getTimings().size())
 				cout << ", ";
 		}
 		cout << endl;
@@ -612,7 +609,7 @@ void SeeExistingDrivers() {
 	cout << "|| # || Condutores EXISTENTES || Horas Diarias || Horas Semamanais || Horas Descanso \n\n";
 
 	for (unsigned int i = 0; i < Drivers.size(); i++) {
-		cout << "||NR - " << (i + 1) << "|| " << Drivers.at(i).getName << "\t  " << Drivers.at(i).getShifts << "\t\t     " << Drivers.at(i).getMaxWeekWorkingTime << "\t\t        " << Drivers.at(i).getMinRestTime << endl;
+		cout << "||NR - " << (i + 1) << "|| " << Drivers.at(i).getName() << "\t  " << Drivers.at(i).getShiftMaxDuration() << "\t\t     " << Drivers.at(i).getMaxWeekWorkingTime() << "\t\t        " << Drivers.at(i).getMinRestTime() << endl;
 	}
 	cout << "\n\n Insira o numero 0 para voltar ao menu principal...\n\n";
 	cin >> exit;
@@ -633,11 +630,11 @@ void LinesStops()
 
 		for (unsigned int i = 0; i < Lines.size(); i++) {
 
-			for (unsigned int f = 0; f < Lines.at(i).getBusStops.size(); f++) {
+			for (unsigned int f = 0; f < Lines.at(i).getBusStops().size(); f++) {
 
-				if (paragem == Lines.at(i).getBusStops.at(f)) {
+				if (paragem == Lines.at(i).getBusStops().at(f)) {
 					cout << "Lista de linhas que passam por esta paragem: " << endl;
-					cout << Lines.at(i).getId << endl;
+					cout << Lines.at(i).getId() << endl;
 					existe = true;
 				}
 			}
@@ -654,7 +651,7 @@ void LinesStops()
 //Functions that deal with the verification of the index of the corresponding vector
 int RetIDLines(unsigned int id) {
 	for (unsigned int i = 0; i < Lines.size(); i++) {
-		if (Lines.at(i).getId == id) {
+		if (Lines.at(i).getId() == id) {
 			return i;
 		}
 	}
@@ -662,7 +659,7 @@ int RetIDLines(unsigned int id) {
 }
 int RetIDDrivers(unsigned int id) {
 	for (unsigned int i = 0; i < Drivers.size(); i++) {
-		if (Drivers.at(i).getId == id) {
+		if (Drivers.at(i).getId() == id) {
 			return i;
 		}
 	}
@@ -723,7 +720,7 @@ int CheckIDLines(unsigned int id) {
 	int flag = 0;
 	unsigned int newid;
 	for (unsigned int i = 0; i < Lines.size(); i++) {
-		if (Lines.at(i).getId == id) {
+		if (Lines.at(i).getId() == id) {
 			flag = 1;
 			newid = i;
 		}
@@ -740,7 +737,7 @@ int CheckIDDrivers(unsigned int id) {
 	int flag = 0;
 	unsigned int newid;
 	for (unsigned int i = 0; i < Drivers.size(); i++) {
-		if (Drivers.at(i).getId == id) {
+		if (Drivers.at(i).getId() == id) {
 			flag = 1;
 			newid = i;
 		}
